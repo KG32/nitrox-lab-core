a4b0b1973ddf23605ff370648b9922f48f89438f
Kuba Groblewski <kkgroblewski@gmail.com>
2022-10-15 21:10:18 +0200

    MOD fO2 -> EANx refactor

--- src/index.ts
+++ src/index.ts
@@ -38,14 +38,14 @@ class NitroxLabCore {
         return Math[mode](num * pwr) / pwr;
     }
 
-    calcMOD(fO2: number, ppO2Max: number): number {
-        if (!(fO2 > 0)) {
-            throw new Error('Incorrect depth range');
+    calcMOD(eanx: number, ppO2Max: number): number {
+        if (!(eanx > 0 && eanx <= 100)) {
+            throw new Error('Incorrect EANx range');
         }
         if (!(ppO2Max > 1)) {
             throw new Error('Incorrect ppO2Max');
         }
-        let mod = ((ppO2Max * 10) / fO2) - 10;
+        let mod = ((ppO2Max * 10) / (eanx / 100) ) - 10;
         if (this.units === Units.IMPERIAL) {
             mod = this.convDepth(mod, Units.IMPERIAL);
         }
